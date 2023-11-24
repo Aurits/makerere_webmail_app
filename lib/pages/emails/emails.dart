b28f2211b0d819b6cecc7ddf31d06e1332e735c6
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-24 12:07:08 +0000

    Nov 24, 2023, 3:07 PM

--- lib/pages/emails/emails.dart
+++ lib/pages/emails/emails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class EmailsPage extends StatefulWidget {
   const EmailsPage({super.key});
@@ -10,11 +11,34 @@ class EmailsPage extends StatefulWidget {
 class _EmailsPageState extends State<EmailsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text("Emails"),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 0,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.black,
+          statusBarIconBrightness: Brightness.light,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.email,
+              color: Colors.black,
+              size: 50,
+            ),
+            Text("Emails",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                )),
+          ],
+        ),
       ),
     );
   }
