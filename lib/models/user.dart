bb325e82d93fd8692387f0cec72507855721954d
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-24 08:02:32 +0000

    Nov 24, 2023, 11:02 AM

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,4 +3,15 @@ class User {
   final String password;
 
   User({required this.username, required this.password});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      username: json['username'],
+      password: json['password'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'username': username, 'password': password};
+  }
 }
