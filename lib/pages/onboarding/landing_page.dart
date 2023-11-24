aa260f758bfc3b370ab4d48db576ad9d59e2ce87
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-24 06:29:19 +0000

    Nov 24, 2023, 9:29 AM

---
+++ lib/pages/onboarding/landing_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  // ignore: non_constant_identifier_names
+  my_init() async {
+    await Future.delayed(const Duration(seconds: 3));
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacementNamed(context, '/news');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    my_init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 26, 51, 45),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.article_outlined,
+              size: 64,
+              color: Colors.red,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'News App',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.brown,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CircularProgressIndicator(backgroundColor: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
