d896410512dd61144333284dd30fa398c38e9583
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-24 07:46:46 +0000

    Nov 24, 2023, 10:46 AM

--- lib/pages/onboarding/login_page.dart
+++ lib/pages/onboarding/login_page.dart
@@ -37,7 +37,7 @@ class _LoginPageState extends State<LoginPage> {
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w300,
-                color: Colors.black26,
+                color: Colors.black45,
               ),
             ),
             const SizedBox(
@@ -105,7 +105,7 @@ class _LoginPageState extends State<LoginPage> {
                             style: TextStyle(
                               fontSize: 12,
                               fontWeight: FontWeight.bold,
-                              color: Colors.green,
+                              color: Colors.redAccent,
                             ),
                           ),
                         ],
